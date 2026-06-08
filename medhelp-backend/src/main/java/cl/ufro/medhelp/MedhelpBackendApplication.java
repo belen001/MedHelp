46e1a7369e20e6aacc773d36b277eb8ee90a9dc7
Belen Bravo <b.bravo02@ufromail.cl>
2026-06-08 14:52:57 -0400

    chore: inicializar proyecto backend con Spring Boot

---
+++ medhelp-backend/src/main/java/cl/ufro/medhelp/MedhelpBackendApplication.java
@@ -0,0 +1,13 @@
+package cl.ufro.medhelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedhelpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedhelpBackendApplication.class, args);
+	}
+
+}
---
+++ medhelp-backend/src/test/java/cl/ufro/medhelp/MedhelpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ufro.medhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedhelpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
