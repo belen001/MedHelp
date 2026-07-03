21241628b49755b56bcaf43feaa189463529d179
Lorenzo Lopez <l.lopez14@ufromail.cl>
2026-07-03 00:25:07 -0400

    Update widget_test.dart

--- frontend/test/widget_test.dart
+++ frontend/test/widget_test.dart
@@ -13,7 +13,9 @@ import 'package:frontend/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    final apiService = ApiService();
+    final authService = AuthService(apiService);
+    await tester.pumpWidget(MyApp(apiService: apiService, authService: authService));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
